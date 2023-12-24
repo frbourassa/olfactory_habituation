Fixed x_n_parallel magnitude in optimal factor calculation

modelfcts/ideal.py
@@ -76,7 +76,9 @@ def ideal_linear_inhibitor(x_n_par, x_n_ort, x_back, f, factor, **opt):
     return s
 
 
-def compute_optimal_factor(nu_new, moments, dims, odor_gen_fct, gen_args):
+def compute_optimal_factor(
+        nu_new, moments, dims, odor_gen_fct, gen_args, reps=1000
+    ):
     """Optimal reduction factor of the parallel component to minimize
     the L2 distance with new odors. Assuming odor vectors,
     new and background alike, are generated independently by odor_gen_fct
@@ -88,21 +90,41 @@ def compute_optimal_factor(nu_new, moments, dims, odor_gen_fct, gen_args):
     This factor depends on the new concentration, nu_new.
     dims = [n_B, n_R].
     moments = [avgnu, sigma2_nu]
+    reps (int): number of samples scales as reps**2: backgrounds x new odors
     """
     avgnu, sigma2_nu = moments
     n_b, n_r = dims
-    # Compute the average vector. Use 10^5 samples
-    vec_samples = odor_gen_fct([n_r, int(1e5)], *gen_args)
+    # Compute the average odor vector. Use 10^5 samples
+    n_samples = reps**2
+    vec_samples = odor_gen_fct([n_r, n_samples], *gen_args)
     vec_samples = vec_samples / l2_norm(vec_samples, axis=0)
-    mean_vec_element = np.mean(vec_samples)  # All elements equal
+    mean_vec_element = np.mean(vec_samples)  # All elements equal by symmetry
     mean_vec_norm2 = n_r * mean_vec_element**2
+
+    # Compute the average norm^2 of the parallel component of new odors
+    # in a background of n_b odors. Generate new background vectors,
+    # use a subsample of vec_samples above as new odors
+    back_samples = odor_gen_fct([n_r, reps*n_b], *gen_args)
+    back_samples = back_samples / l2_norm(back_samples, axis=0)
+    x_par_norms2 = np.zeros(reps**2)  # Container for all x_{n, par}^2 samples
+    for n in range(reps):
+        back_vecs = back_samples[:, n*n_b:(n+1)*n_b]
+        proj = find_projector(back_vecs)
+        # Take some of the previously generated samples as new odors
+        for m in range(n*reps, (n+1)*reps):
+            x_par = find_parallel_component(
+                        vec_samples[:, m], back_vecs, projector=proj
+                    )
+            x_par_norms2[m] = np.sum(x_par**2)
+    x_par_norm2 = np.mean(x_par_norms2)
 
     # Compute terms that appear in the optimum solution
     cross_product = nu_new * avgnu * n_b * mean_vec_norm2
     mean_back2 = avgnu**2 * (n_b + n_b*(n_b-1)*mean_vec_norm2) + n_b*sigma2_nu
+    mean_par2 = x_par_norm2 * nu_new**2
 
-    optimal_factor = cross_product + nu_new**2
-    optimal_factor /= (mean_back2 + 2*cross_product + nu_new**2)
+    optimal_factor = cross_product + mean_par2
+    optimal_factor /= (mean_back2 + 2*cross_product + mean_par2)
     return optimal_factor
 
 
@@ -111,6 +133,7 @@ def compute_optimal_factor_toy(nu_new, sigma2, n_r, odor_gen_fct, gen_args):
     $x_B = x_d + \nu x_s$.
     """
     # Compute the average vector. Use 10^5 samples
+    # TODO: fix with an estimate of x_n^2 norm
     vec_samples = odor_gen_fct([n_r, int(1e5)], *gen_args)
     vec_samples = vec_samples / l2_norm(vec_samples, axis=0)
     mean_vec_element = np.mean(vec_samples)  # All elements equal