Checked with linear analysis that the sharp drop in performance as Lambda increases comes from numerical instability of the Euler integrator, not from a bifurcation to real instability of the fixed point

modelfcts/biopca.py
@@ -106,7 +106,7 @@ def integrate_inhib_ifpsp_network_skip(ml_inits, update_bk, bk_init,
         xmean_series, cbar_series, w_series, s_series
     """
     remove_mean = model_options.get("remove_mean", False)
-    remove_lambda = model_options.get("remove_lambda", True)
+    remove_lambda = model_options.get("remove_lambda", False)
     activ_fct = str(model_options.get("activ_fct", "ReLU")).lower()
     m_init, l_init = ml_inits
     # Note: keep lambda matrix as 1d diagonal only, replace dot products by:
@@ -230,10 +230,10 @@ def integrate_inhib_ifpsp_network_skip(ml_inits, update_bk, bk_init,
         c = inv_l_diag * (mmat.dot(bkvec - xmean))  # L_d^(-1) M^T x
         # Lateral inhibition between neurons
         cbar = c - inv_l_diag*np.dot(lmat - dflt(1.0/inv_l_diag), c)
-        # Remove the Lambda scale of eigenvectors, so the W matrix does
-        # not need to compensate too much.
-        # So we use Lambda^{-1}L^{-1}M as a projector as prescribed in Minden 2018
         if remove_lambda:
+            # Remove the Lambda scale of eigenvectors, so the W matrix does
+            # not need to compensate too much.
+            # So we use Lambda^{-1}L^{-1}M as a projector as prescribed in Minden 2018
             cbar = cbar / lambda_diag
 
         # Lastly, projection neurons at time step k+1.

modelfcts/checktools.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from modelfcts.distribs import truncexp1_density
-from utils.metrics import subspace_align_error
+from utils.metrics import subspace_align_error, l2_norm
 
 
 # Function to check the s.s. distribution of the power-law and exp1 process
@@ -201,6 +201,70 @@ def compute_ibcm_second_derivative(m_fix, theta_fix, ibcm_params, back_moments):
 
     # Return f and Df
     return f_vec, df
+
+
+def jacobian_row_wmat_l2_avgstats(cgammas, back_moments, w_rates):
+    """ Jacobian of dW/dt, given M and statistics of the background.
+    All rows of W have the same jacobian, the full ijxij jac is block-diagonal
+    So only compute the jacobian for one row derived wrt the same row.
+    This is to check stability of the W fixed point (irrespective of numerics)
+
+    Args:
+        cgammas (np.ndarray): matrix of dot products of L.M with each
+            background component x_gamma, indexed [i, gamma]
+        back_moments (list of floats of np.ndarrays): average nu, sigma^2
+            of all components (if floats) or of each component gamma (arrays)
+        w_rates (list of floats): alpha, beta
+    Returns:
+        jac (np.ndarray): jacobian matrix of one row of W by itself,
+            which is one block of the Jacobian for the full W matrix
+    """
+    avgnu, sigma2s = back_moments
+    alpha, beta = w_rates
+    n_i = cgammas.shape[0]
+    # Compute vector of c_ds: c^I_d
+    c_d = avgnu * np.sum(cgammas, axis=1)
+    c_dmat = c_d[:, None] * c_d[None, :]
+    # Compute matrix of c_gamma products, summed over gamma
+    c_gamma_mat = np.sum(sigma2s * cgammas[:, None, :]*cgammas[None, :, :], axis=2)
+    #  Compute <c^j c^l> correlations.
+    ccmat = c_dmat + c_gamma_mat
+    # Compute jacobian
+    jac = -beta * np.identity(n_i) - alpha * ccmat
+    return jac
+
+
+def stability_row_wmat_l2_instant(cvec, w_rates, dt):
+    """ Compute the eigenvalues of the Euler integrator's iterative map for W,
+    given an instantaneous \bar{c} value, the inhibitory neurons' activities.
+    Imagining that the c's remain constant thereafter, is the Euler integrator
+    on a stable tangent for this step? Check by computing the eigenvalues
+    of the matrix multiplying W in this iterative map,
+    W^{t+1} = W^t + dt * alpha*x.c^T - dt*(alpha*c.c^T + beta*identity).W
+    so the matrix of which to check eigenvalues and ensure they are < 1 is
+        A = 1 - dt*(alpha*c.c^T + beta*identity)
+
+    Args:
+        cvec (np.ndarray): vector of inhibitory neurons' activities,
+            i.e. LMx
+        w_rates (list of floats): alpha, beta
+        dt (float): time step
+
+    Returns:
+        eigvals (np.ndarray): eigenvalues of the linear update operator,
+            that is, 1 + dt*jacobian.
+    """
+    # Symmetric matrix, can use eigvalsh
+    alpha, beta = w_rates
+    n_i = cvec.shape[0]
+    #up_mat = np.identity(n_i)*(1.0 - dt*beta) - dt*alpha*np.outer(cvec, cvec)
+    #eigvals = np.linalg.eigvalsh(up_mat)
+    # Obviously, with the outer product here, one eigenvalue is
+    # 1-dt*(beta+alpha*cvec**2), the others are 1-beta*dt
+    # So no need to compute the whole matrix and diagonalize.
+    eigvals = np.full(n_i, fill_value=1.0 - dt*beta)
+    eigvals[0] = 1.0 - dt*beta - dt*alpha*l2_norm(cvec)**2
+    return eigvals
 
 
 ### Functions to analyze biologically plausible online PCA results

modelfcts/ibcm.py
@@ -428,9 +428,9 @@ def integrate_inhib_ibcm_network_options(vari_inits, update_bk, bk_init,
         # In principle, should add low decay to background subspace
         # To make sure 1) only learn the background space, 2) de-habituate after
         if decay and variant == "law":
-            m -= dt * decay_relative * learnrate / (ktheta + cbar2_avg[:, np.newaxis]/lambd) * m
+            m -= dt * decay_relative * mu_abs / (ktheta + cbar2_avg[:, np.newaxis]/lambd) * m
         elif decay and variant == "intrator":
-            m -= dt * decay_relative * learnrate * m
+            m -= dt * decay_relative * mu_abs * m
         # Now, update to time k+1 the threshold (cbar2_avg) using cbar at time k
         # to be used to update m in the next time step
         cbar2_avg += dt * (cbar*cbar / lambd - cbar2_avg)/tavg

simulfcts/plotting.py
@@ -401,7 +401,7 @@ def plot_cbars_gamma_series(ts, cbg_ser, skp=200, transient=None):
 
 ### PLOTS SPECIFIC TO ONLINE PCA ###
 def plot_pca_results(tser, truepca, learntpca, alignerrser, off_diag_l_ser,
-                     palette1=None, palette2=None):
+                     palette1=None, palette2=None, skp=1):
     """
     Args:
         tser (np.ndarray): time steps
@@ -414,6 +414,7 @@ def plot_pca_results(tser, truepca, learntpca, alignerrser, off_diag_l_ser,
         palette1 (list of colors): colors for the various principal values
         palette2 (list of 2 colors): palette for the plots of error vs time
             and L elements vs time.
+        skp (int): plot only every skp time point
 
     Returns:
         fig, axes (list of fig, ax): fig and axes of principal values vs time,
@@ -429,8 +430,9 @@ def plot_pca_results(tser, truepca, learntpca, alignerrser, off_diag_l_ser,
     fig.set_size_inches(default_size[0], default_size[1]*1.33)
     n_comp = learntpca[0].shape[1]
     for i in range(n_comp):
-        li, = ax.plot(tser, learntpca[0][:, i], label="Learnt {}".format(i),
-                      lw=1.0, zorder=10-i, color=palette1[i])
+        li, = ax.plot(tser[::skp], learntpca[0][::skp, i],
+                    label="Learnt {}".format(i),
+                    lw=1.0, zorder=10-i, color=palette1[i])
         if truepca[0][i] / truepca[0].max() > 1e-12:
             ax.axhline(truepca[0][i], ls="--", color=palette1[i], lw=1.0 - i/n_comp,
                 label="True {}".format(i), zorder=n_comp-i)
@@ -439,12 +441,12 @@ def plot_pca_results(tser, truepca, learntpca, alignerrser, off_diag_l_ser,
     leg.set_zorder(30)
 
     ax = axes[1]
-    ax.plot(tser, alignerrser, color=palette2[0])
+    ax.plot(tser[::skp], alignerrser[::skp], color=palette2[0])
     ax.set(yscale="log", ylabel="Subspace alignment error")
 
     ax = axes[2]
-    ax.plot(tser, np.mean(learntpca[0], axis=1), label="Diagonal", color=palette2[0])
-    ax.plot(tser, off_diag_l_ser, label="Off-diagonal", color=palette2[1])
+    ax.plot(tser[::skp], np.mean(learntpca[0][::skp], axis=1), label="Diagonal", color=palette2[0])
+    ax.plot(tser[::skp], off_diag_l_ser[::skp], label="Off-diagonal", color=palette2[1])
     ax.set(yscale="log", ylabel=r"Average $L_{ij}$ magnitude", xlabel="Time (steps)")
     ax.legend()
     fig.tight_layout()